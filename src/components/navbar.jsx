42063b0a41ac60722551730f9a99a566b1041368
Patra Sawali <patrasawali93@gmail.com>
2025-10-22 10:47:25 +0700

    Update navbar.jsx
    changeee

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -32,7 +32,7 @@ const Navbar = () => {
                 {mobileMenu ? '✕' : '☰'}
             </button>
 
-            <ul className={`menu md:flex items-center sm:gap-10 gap-4 fixed md:static top-0 right-0 h-screen md:h-auto w-[60%] md:w-auto flex-col md:flex-row justify-center bg-zinc-900/95x md:bg-transparent backdrop-blur-md transition-all duration-300 ease-in-out md:translate-x-0 ${mobileMenu ? 'translate-x-0' : 'translate-x-full'} md:opacity-100`}>
+            <ul className={`menu md:flex items-center sm:gap-10 gap-4 fixed md:static top-0 right-0 h-screen md:h-auto w-[60%] md:w-auto flex-col md:flex-row justify-center  sm:bg-zinc-800 bg-zinc-900/95x md:bg-transparent backdrop-blur-md transition-all duration-300 ease-in-out md:translate-x-0 ${mobileMenu ? 'translate-x-0' : 'translate-x-full'} md:opacity-100`}>
                 <li>
                     <a onClick={() => setMobileMenu(false)} href="#Home" className="sm:text-lg text-base font-medium px-4 py-2 rounded-md transition-all duration-300 md:hover:bg-violet-500 block">Home</a>
                 </li>
